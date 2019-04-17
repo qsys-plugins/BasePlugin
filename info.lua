0b6b54142fb6141e8702bac8153bacab32e87231
Amanda Abate <amanda.abate@qsc.com>
2019-04-17 15:43:25 -0600

    submodule success, build script always builds plugin.lua

--- info.lua
+++ info.lua
@@ -1,6 +1,6 @@
 PluginInfo = {
-    Name = "Compile Test 3",
-    Version = "0.0.0.1",
+    Name = "Compile Test 4",
+    Version = "0.0.0.3",
     Id = "COMPILEDPLUGINTEST",
     Description = "Compiled from Visual Studio Code",
     ShowDebug = false
