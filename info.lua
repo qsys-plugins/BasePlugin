7c09e3f6561c482d199d0c4b4eab803241fb682e
Amanda Abate <amanda.abate@qsc.com>
2019-04-17 17:19:56 -0600

    commit test 10

--- info.lua
+++ info.lua
@@ -1,6 +1,6 @@
 PluginInfo = {
     Name = "Compile Test 4",
-    Version = "0.1.2.3",
+    Version = "0.1.2.5",
     Id = "COMPILEDPLUGINTEST",
     Description = "Compiled from Visual Studio Code",
     ShowDebug = false
