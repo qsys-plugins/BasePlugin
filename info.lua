57777a03ea4017f32f1359c74943fa12e2d96cf0
Amanda Abate <amanda.abate@qsc.com>
2019-04-18 17:45:04 -0600

    moved compile/copy scripts to plugin module, updated task

--- info.lua
+++ info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
     Name = "Test~Compile Test 6",
-    Version = "2.2.0.8",
-    Id = "COMPILEDPLUGIN-Test~CompileTest6-2.2.0.8",
+    Version = "2.2.0.9",
+    Id = "COMPILEDPLUGIN-Test~CompileTest6-2.2.0.9",
     Description = "Compiled from Visual Studio Code",
     ShowDebug = false
 }
