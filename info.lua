aa4a175ab8db390d7104e3d908b4288c90e19455
Amanda Abate <amanda.abate@qsc.com>
2019-04-18 18:49:55 -0600

    first compile

--- info.lua
+++ info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
     Name = "Base Plugin~Base Plugin",
-    Version = "0.0.0.0",
-    Id = "COMPILEDPLUGIN-BasePlugin-0.0.0.0",
+    Version = "0.0.0.1",
+    Id = "COMPILEDPLUGIN-BasePlugin-0.0.0.1",
     Description = "Compiled from Visual Studio Code",
     ShowDebug = true
 }
