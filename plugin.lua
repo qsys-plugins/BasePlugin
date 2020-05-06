6a537df17a9d2b414aecbd8458c553c24f013da6
Caitlyn Sullivan <caitlyn.sullivan@qsc.com>
2020-05-06 12:57:27 -0600

    updated tasks, compiler, and runtime

--- plugin.lua
+++ plugin.lua
@@ -1,5 +1,5 @@
 -- Base Plugin
--- by QSC
+-- by
 -- Month 20YY
 
 --[[ #include "info.lua" ]]
@@ -12,42 +12,40 @@ function GetPrettyName(props)
   return "Base Plugin " .. PluginInfo.Version
 end
 
-function GetPages()
+--[=[]]
+function GetPages(props) --optional function if plugin has multiple pages
   pages = {}
-
   --[[ #include "pages.lua" ]]
-
   return pages
 end
 
+function GetModel(props) --optional function to define model if plugin supports more than one model
+  model = {}
+  --[[ #include "model.lua" ]]
+  return model
+end
+--]=]
+
 function GetProperties()
   props = {}
-
   --[[ #include "properties.lua" ]]
-
   return props
 end
 
 function GetControls(props)
   ctrls = {}
-
   --[[ #include "controls.lua" ]]
-
   return ctrls
 end
 
 function GetControlLayout(props)
   layout   = {}
   graphics = {}
-
   --[[ #include "layout.lua" ]]
-
   return layout, graphics
 end
 
 --Start event based logic
 if Controls then
-
   --[[ #include "runtime.lua" ]]
-  
 end
